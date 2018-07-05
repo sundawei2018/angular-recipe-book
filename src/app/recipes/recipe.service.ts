d5cc4340f73c741eb1cde93778ba7abcf426a622
sundawei2018 <dsun2016@gmail.com>
2018-07-05 00:12:02 -0400

    add ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,16 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe1', 'This is a simple test', 'https://images.media-allrecipes.com/images/56589.png'),
-    new Recipe('A Test Recipe2', 'Another test', 'https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg')
+    new Recipe('A Test Recipe1', 
+               'This is a simple test', 
+               'https://images.media-allrecipes.com/images/56589.png',
+               [new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)]),
+    new Recipe('A Test Recipe2', 
+               'Another test', 
+               'https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg',
+               [new Ingredient('Buns', 2),
+               new Ingredient('Meat',1)])
 
   ];
 
