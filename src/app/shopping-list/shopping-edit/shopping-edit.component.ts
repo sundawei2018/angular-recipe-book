425b0bab980bca4f564a2fba8f7aaf4963ef728c
sundawei2018 <dsun2016@gmail.com>
2018-07-05 22:49:27 -0400

    add edit shopping list form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -39,7 +39,13 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     const value = form.value;
     console.log(value);
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.slService.addIngredient(newIngredient);
+
+    if (this.editMode) {
+      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+    }
+    else {
+      this.slService.addIngredient(newIngredient);
+    }
   }
 
   ngOnDestroy() {
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -32,5 +32,10 @@ export class ShoppingListService {
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 
+  updateIngredient(index : number, newIngredient : Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
   constructor() { }
 }
