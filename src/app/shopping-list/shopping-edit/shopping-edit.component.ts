02045b1a6c0ad1d02670bb9d1020aefa866969eb
sundawei2018 <dsun2016@gmail.com>
2018-07-05 22:57:51 -0400

    add cancel function

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -46,6 +46,13 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     else {
       this.slService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    form.reset();
+  }
+
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
   }
 
   ngOnDestroy() {
